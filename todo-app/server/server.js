f5c07b83ae46a38ed9cdf1392ed65d3fa1fe034b
hemanthbanala <141825417+hemanthbanala@users.noreply.github.com>
2025-08-25 18:04:22 +0530

    Update server.js

--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -14,6 +14,6 @@ app.get('/', (req, res) => {
   res.send('To-Do List API is running');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
